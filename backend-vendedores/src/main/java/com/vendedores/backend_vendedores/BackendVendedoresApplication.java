e743a6b0b2430d59cfebc8633e758a39e7344840
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-08-13 16:42:48 -0300

    Closes #1 - projeto spring

---
+++ backend-vendedores/src/main/java/com/vendedores/backend_vendedores/BackendVendedoresApplication.java
@@ -0,0 +1,13 @@
+package com.vendedores.backend_vendedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVendedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVendedoresApplication.class, args);
+	}
+
+}
---
+++ backend-vendedores/src/test/java/com/vendedores/backend_vendedores/BackendVendedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendedores.backend_vendedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVendedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
